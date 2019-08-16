fbc2906f102b88564ccc3488653b940f0f6bbcb2
Felipe Freitas <fp4303@gmail.com>
2019-08-16 20:24:16 +0800

    BST part2

--- Binary search tree/Binary_search_tree.js
+++ Binary search tree/Binary_search_tree.js
@@ -3,45 +3,19 @@ var tree;
 function setup() {
 	noCanvas();
 	tree = new Tree();
-	tree.addValue(5);
-	tree.addValue(3);
-	tree.addValue(7);
-	tree.addValue(6);
+	for (var i =0; i < 10; i++){
+		tree.addValue(floor(random(0,100)));
+	}
+	console.log(tree);
+	tree.traverse();
 
-	console.log(tree)
-}
-
-function Tree() {
-	this.root = null;
-}
-
-Tree.prototype.addValue = function(val) {
-	var n = new Node(val);
-	if (this.root == null) {
-		this.root = n;
+	var result = tree.search(11);
+	if (result == null) {
+		console.log('not found');
 	} else {
-		this.root.addNode(n)
+		console.log(result)
 	}
 }
 
-Node.prototype.addNode = function(n) {
-	if (n.value < this.value){
-		if (this.left == null){
-			this.left = n;
-		} else {
-			this.left.addNode(n);
-		}
-	} else if (n.value > this.value){
-		if (this.right == null) {
-			this.right = n;
-		} else {
-			this.right.addNode(n);
-		}
-	}
-}
 
-function Node(val) {
-	this.value = val;
-	this.left = null;
-	this.right = null;
-}
+
